aa9885ffddeaf2d092e7ff4c57f1d86128575f7e
Abhishek Mishra <abhishekmishra9051@gmail.com>
2023-10-16 20:22:13 +0530

    File added

---
+++ employee-management-system/src/main/java/com/coderletscode/employee/services/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.coderletscode.employee.services;
+
+import com.coderletscode.employee.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    Employee createEmployee(Employee employee);
+
+    List<Employee> getAllEmployees();
+
+    boolean deleteEmployee(Long id);
+
+    Employee getEmployeeById(Long id);
+
+    Employee updateEmployee(Long id, Employee employee);
+}
